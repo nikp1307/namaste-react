0dc42e0ed9c91145fc0ada112e2d2568936cfc73
nikp1307 <monikhilpanda@gmail.com>
2024-02-06 22:56:36 -0700

    laying the foundation

--- App.js
+++ App.js
@@ -2,7 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // React Element
-const jsxHeading = <h1 id="heading">Heading using JSX!</h1>;
-console.log(jsxHeading);
+const jsxHeading = (
+  <h1 className="root" tabIndex="5">
+    Heading using JSX Element!
+  </h1>
+);
+
+// React Functional Component
+
+const TitleComponent = () => (
+  <h1 className="head" tabIndex="5">
+    Heading using JSX!
+  </h1>
+);
+
+const HeadingComponent = () => (
+  <div id="container">
+    {jsxHeading}
+    <TitleComponent />
+    <h1 className="heading">Heading using React Functional Component!</h1>
+  </div>
+);
+
+const HeadingComponent2 = () => {
+  return <h1>Heading using React Functional Component!</h1>;
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent></HeadingComponent>);
